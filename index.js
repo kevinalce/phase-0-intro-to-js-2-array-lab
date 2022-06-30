6da3929c0c924785eb4f49cc647ff31da727075f
Kevin Alce <Kevinalce33@gmail.com>
2022-06-30 17:36:45 -0400

    commit tree 2fc1105f6719904e7b77d48706b4e5c5845c582e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name2){
+    cats.unshift(name2);
+}
+function destructivelyRemoveLastCat(last){
+    cats.pop(last);
+
+}
+function destructivelyRemoveFirstCat(first){
+    cats.shift(first);
+}
+function appendCat(name3){
+    return appendCat = [...cats, name3];
+}
+function prependCat(name4){
+    return prependCat = [name4, ...cats];
+}
+function removeLastCat(name5){
+    return removeLastCat = [...cats.slice(0,2)];
+}
+function removeFirstCat(name6){
+    return removeFirstCat = [...cats.slice(1,3)]
+}
